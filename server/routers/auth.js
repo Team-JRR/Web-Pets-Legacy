1ee374b53b070eabae1c3cd386f321a1c36e6448
azaria <azariaplayer@gmail.com>
2025-12-17 11:42:27 -0600

    Configuring strategy for Passportjs

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -5,8 +5,53 @@ const db = require('../db');
 
 passport.use(new GoogleStrategy({
   clientID: process.env['GOOGLE_CLIENT_ID'],
-
-}));
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/google',
+  scope: ['profile']
+},
+  function verify(issuer, profile, cb) {
+    db.Credential.find({provider: issuer, subject: profile.id})
+      .then((credentials) => {
+        if(!credentials.length){
+          db.User.create({name: profile.displayName})
+            .then((newUser) => {
+              db.Credential.create({userId: newUser._id, provider: issuer, subject: profile.id})
+                .then(() => {
+                  const user = {
+                    id: newUser._id,
+                    name: profile.displayName
+                  };
+                  return cb(null, user);
+                })
+                .catch((err) => {
+                  console.error(err);
+                  return cb(err);
+                });
+            })
+            .catch((err) => {
+              console.error(err);
+              return cb(err);
+            });
+        } else {
+          db.User.findById(credentials[0].userId)
+            .then((user) => {
+              if(!user){
+                return cb(null, false);
+              }
+              return cb(null, user);
+            })
+            .catch((err) => {
+              console.error(err);
+              return cb(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return cb(err);
+      });
+  }
+));
 
 const router = express.Router();
 
