ec4c096c50f8b0b7dad9889ecdb41de1106e07b8
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-01-02 16:52:34 -0600

    ignore docs/ and main.js when linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, rules: {semi: ["error", "always"], "no-console": ["error", {allow: ["warn", "info", "error"]}]} },
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended, globalIgnores(['docs/', 'client/dist'])
 ]);
