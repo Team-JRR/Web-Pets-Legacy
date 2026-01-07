b8f9cb8055655338d107ade5c80f6d102eef1840
JaylinJones0 <jonesjaylink@gmail.com>
2026-01-07 08:58:07 -0600

    (update) userSchema to expect a device color, set to sky blue hex as default, and only accepts valid hex code strings

--- server/db/user.js
+++ server/db/user.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
+const colorValidator = (v) => { return /^#?([0-9a-f]{3}|[0-9a-f]{6})$/i.test(v); };
+
 const userSchema = new mongoose.Schema({
   name: String,
   petsAdopted: Number,
   petsDisappeared: Number,
-  status: String
+  status: String,
+  deviceColor: { type: String, default: '#87cefa', validate: { validator: colorValidator, message: props => `${props.value} is not a valid hex color format!`}, require: true}
 });
 
 const User = mongoose.model('User', userSchema);
