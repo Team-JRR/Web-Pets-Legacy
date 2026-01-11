2f54ff1726d22ab1b2929e382fc51f63cece1773
RRARI504 <ryansimien@ymail.com>
2026-01-10 22:34:49 -0600

    "feat (archive user) fixed profile page, front end renders from passed down pet data, get req not needed"

--- client/src/components/ProfileView.jsx
+++ client/src/components/ProfileView.jsx
@@ -6,42 +6,20 @@ import axios from 'axios';
 
 
 const ProfileView = ({pet}) => {
-  //state of pet detail
-  const [petDetails, setPetDetails] = useState(null);
-  //view triggers READ to the pet by id
-  useEffect(() =>{
-    if(!pet){
-      return;
-    }
-    //fetches latest data
-    axios.get(`/pet/${pet._id}`)
-    .then((res) =>{
-      //set state of pet details
-      setPetDetails(res.data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }, [pet])
-
-
-  if(!petDetails){
-    return <p>LOADING</p>
-  }
-
-
-
+  
 
   return (
      <div className="border-2 border-black p-[10px]">
-      <p><strong>Name:</strong> {petDetails.name}</p>
-      <p><strong>Mood:</strong> {petDetails.mood}</p>
-      <p><strong>Love:</strong> {petDetails.love}</p>
-      <p><strong>Health:</strong> {petDetails.health}</p>
-      <p><strong>Hunger:</strong> {petDetails.hunger}</p>
+  
+      <p><strong>Name:</strong> {pet.name}</p>
+      <p><strong>Mood:</strong> {pet.mood}</p>
+      <p><strong>Love:</strong> {pet.love}</p>
+      <p><strong>Health:</strong> {pet.health}</p>
+      <p><strong>Hunger:</strong> {pet.hunger}</p>
 
       <h4>Training</h4>
       <ul>
-        {petDetails.training.map(skill => (
+        {pet.training.map(skill => (
           <li key={skill.name}>
             {skill.name}: {skill.stat}
           </li>
