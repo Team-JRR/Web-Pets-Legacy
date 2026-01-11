707fcdae7568832d2caf0b4135e47be9615e2091
JaylinJones0 <jonesjaylink@gmail.com>
2026-01-11 16:58:48 -0600

    (update) hex code input field clears after clicking apply

--- client/src/components/Dashboard/Profile.jsx
+++ client/src/components/Dashboard/Profile.jsx
@@ -11,34 +11,36 @@ const Profile = ({ contrastTB, refreshDeviceColorData }) => {
     setHexInput(e.target.value);
   };
 
-
   // handles submitting the hex code user entered to be saved to their database profile
-  // after that the refreshDeviceColorData method is called which causes a rerender of the App component, 
+  // after that the refreshDeviceColorData method is called which causes a rerender of the App component,
   // which then rerenders the DeviceViews componenet allowing it to see the updated color immediately.
   const handleHexSubmit = () => {
-    axios.patch(`/user/device-color`, {
-        deviceColor: hexInput
-    })
-    .then(() => {
+    axios
+      .patch(`/user/device-color`, {
+        deviceColor: hexInput,
+      })
+      .then(() => {
         refreshDeviceColorData();
-    })
-    .catch(err => {
+        setHexInput("");
+      })
+      .catch((err) => {
         console.error("Failed to update Device Color", err);
-    });
+      });
   };
 
   // triggers patch handler in server/routers/user.js, when it is passed undefined as the device color value
   // it sets the value of the currently logged in User's device color back to default (hardcoded value in the handler)
   const handleRevert = () => {
-    axios.patch(`/user/device-color`, {
-        deviceColor: undefined
-    })
-        .then(() => {
-            refreshDeviceColorData();
-        })
-        .catch(err => {
-            console.error("Failed to revert to default device color", err);
-        });
+    axios
+      .patch(`/user/device-color`, {
+        deviceColor: undefined,
+      })
+      .then(() => {
+        refreshDeviceColorData();
+      })
+      .catch((err) => {
+        console.error("Failed to revert to default device color", err);
+      });
   };
 
   return (
@@ -52,10 +54,18 @@ const Profile = ({ contrastTB, refreshDeviceColorData }) => {
         minLength={4}
         maxLength={7}
       />
-      <button className={profileContentStyles.join(" ")} onClick={handleHexSubmit} type="button">
+      <button
+        className={profileContentStyles.join(" ")}
+        onClick={handleHexSubmit}
+        type="button"
+      >
         Apply Change
       </button>
-            <button className={profileContentStyles.join(" ")} onClick={handleRevert} type="button">
+      <button
+        className={profileContentStyles.join(" ")}
+        onClick={handleRevert}
+        type="button"
+      >
         Revert Changes
       </button>
     </div>
